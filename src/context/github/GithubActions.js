57dc55a9704a1fed24fe8bec63a5c9babe0023d4
tnjkt <naujokaitetoma@gmail.com>
2022-02-24 11:20:29 +0200

    token-fix

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -2,12 +2,14 @@ import axios from "axios";
 
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 
-// const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 const github = axios.create({
   baseURL: GITHUB_URL,
-  // headers: {Authorization: `token ${GITHUB_TOKEN}`}
+  headers: { Authorization: `Bearer ${GITHUB_TOKEN}` },
 });
+
+console.log(GITHUB_TOKEN);
 
 // get search results
 export const searchUsers = async (text) => {
